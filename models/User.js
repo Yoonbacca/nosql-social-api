ca5af82c6e21e7a1f47766bd5b8fce1e850003bf
YoonJae Kim <yoonjaekim3@gmail.com>
2023-10-12 13:40:09 -0500

    Created user model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+const thoughtSchema = require("./Thought");
+import { isEmail } from "validator";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: [isEmail, "Invalid Email"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
